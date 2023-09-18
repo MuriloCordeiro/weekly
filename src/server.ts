d40be65e9eeeb74604db428e2eaedd2b908018ea
Murilo <murilo.ramos@rspneus.com.br>
2023-09-17 21:35:43 -0300

    commit for cors

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,18 @@ import express from "express";
 import router from "./routes";
 import { Mongoose } from "mongoose";
 import connectDB from "./db/mongoose";
+import cors from "cors";
 
 const server = express();
 // const router = Router();
 
 const port = 3333;
-
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+server.use(cors(corsOptions));
 server.use(express.json());
 
 server.use(router);
