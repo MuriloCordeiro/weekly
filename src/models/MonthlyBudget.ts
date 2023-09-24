c86ae3840bb2959d807d2c2842f2d8759237ca4a
Murilo <murilo.ramos@rspneus.com.br>
2023-09-23 22:04:17 -0300

    chore: commit changes

--- src/models/MonthlyBudget.ts
+++ src/models/MonthlyBudget.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 //   semana: String,
 // });
 const expenseSchema = new mongoose.Schema({
+  title: String,
   description: String,
   value: Number,
   expenseDate: String,
