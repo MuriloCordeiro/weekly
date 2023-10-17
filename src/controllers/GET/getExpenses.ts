728a77658fb846e200fd472ac8c735824137d3f2
jcwisniewski <julio.wisnieski@gmail.com>
2023-10-17 14:16:50 -0300

    added filter logic

--- src/controllers/GET/getExpenses.ts
+++ src/controllers/GET/getExpenses.ts
@@ -2,63 +2,92 @@ import { Request, Response } from "express";
 import Expenses from "../../models/Expenses";
 import FormatDateBrazilian from "../../utils/formatDateBrazilian";
 
-//Função para formatar datas para "DD/MM/YYYY"
-
 export async function GetExpenses(req: Request, res: Response) {
-  const { userId, parameter, value, startDate, endDate } = req.query;
+  const { userId, typeExpense, startDate, endDate } = req.query;
 
   if (!userId) {
     return res.status(400).send("O userId é obrigatório.");
   }
 
   try {
-    let matchQuery: Record<string, any> = { userId }; // Objeto de consulta com base no userId
+    const startDateFormatted: Date = new Date(
+      FormatDateBrazilian(String(startDate))
+    );
+    const endDateFormatted: Date = new Date(
+      FormatDateBrazilian(String(endDate))
+    );
 
-    if (parameter === "type") {
-      // Adicione o critério de pesquisa por tipo
-      matchQuery["expenses.type"] = value;
-    }
+    // Define um pipeline de agregação com as etapas de match e project
 
-    if (startDate && endDate) {
-      const startDateFormatted: Date = new Date(
-        FormatDateBrazilian(String(startDate))
-      );
-      const endDateFormatted: Date = new Date(
-        FormatDateBrazilian(String(endDate))
-      );
+    let pipeline: any = undefined;
 
-      console.log("startDateFormatted", startDateFormatted);
-      console.log("endDateFormatted", endDateFormatted);
-
-      console.log("startDate", startDate);
-      // Adicione o critério de pesquisa por intervalo de datas
-      matchQuery["$and"] = [
+    if (typeExpense !== "undefined") {
+      pipeline = [
         {
-          "expenses.createdAt": { $gte: startDateFormatted },
+          $match: {
+            userId: userId,
+            "expenses.createdAt": {
+              $gte: startDateFormatted,
+              $lte: endDateFormatted,
+            },
+            "expenses.type": { $eq: typeExpense },
+          },
         },
         {
-          "expenses.endDate": { $lte: endDateFormatted },
+          $project: {
+            expenses: {
+              $filter: {
+                input: "$expenses",
+                as: "expense",
+                cond: {
+                  $and: [
+                    { $gte: ["$$expense.createdAt", startDateFormatted] },
+                    { $lte: ["$$expense.createdAt", endDateFormatted] },
+                    { $eq: ["$$expense.type", typeExpense] },
+                  ],
+                },
+              },
+            },
+          },
         },
       ];
-
-      console.log("query", matchQuery);
+    } else {
+      pipeline = [
+        {
+          $match: {
+            userId: userId,
+            "expenses.createdAt": {
+              $gte: startDateFormatted,
+              $lte: endDateFormatted,
+            },
+          },
+        },
+        {
+          $project: {
+            expenses: {
+              $filter: {
+                input: "$expenses",
+                as: "expense",
+                cond: {
+                  $and: [
+                    { $gte: ["$$expense.createdAt", startDateFormatted] },
+                    { $lte: ["$$expense.createdAt", endDateFormatted] },
+                  ],
+                },
+              },
+            },
+          },
+        },
+      ];
     }
 
-    // Execute a consulta no banco de dados usando o método aggregate
-    const result = await Expenses.aggregate([
-      { $match: matchQuery }, // Filtre com base no userId, tipo e intervalo de datas
-      { $project: { _id: 0, expenses: 1 } }, // Projete apenas o campo "expenses"
-    ]);
-
+    // Execute a agregação e aguarde a conclusão
+    const result = await Expenses.aggregate(pipeline).exec();
     if (result.length === 0) {
-      return res
-        .status(404)
-        .send("Nenhuma despesa encontrada com os critérios especificados.");
+      res.status(404).send("Não foram encontradas despesas com estes filtros.");
+    } else {
+      res.status(200).send(result);
     }
-
-    const expenses = result[0].expenses; // Acesse o array de despesas
-
-    res.status(200).send(expenses);
   } catch (error) {
     console.error("Erro ao obter despesas:", error);
     res.status(500).send("Erro interno do servidor");
