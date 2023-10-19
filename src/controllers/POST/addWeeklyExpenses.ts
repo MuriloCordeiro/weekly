d43841a77178486f2a34113a05dabe8014a347a7
Murilo <murilo.ramos@rspneus.com.br>
2023-10-19 10:26:22 -0300

    chore: commit for merge

--- src/controllers/POST/addWeeklyExpenses.ts
+++ src/controllers/POST/addWeeklyExpenses.ts
@@ -20,7 +20,7 @@ export async function AddWeeklyExpenses(req: Request, res: Response) {
     !value ||
     !expenseDate
   ) {
-    return res.status(400).send("Todos os campos são obrigatórios.");
+    return res.status(400).send({ res: "Todos os campos são obrigatórios." });
   }
 
   try {
@@ -29,7 +29,7 @@ export async function AddWeeklyExpenses(req: Request, res: Response) {
     if (!userBudget) {
       return res
         .status(404)
-        .send("Orçamento não encontrado para o usuário especificado.");
+        .send({ res: "Orçamento não encontrado para o usuário especificado." });
     }
 
     let budget: any = userBudget.budget.find(
@@ -39,17 +39,19 @@ export async function AddWeeklyExpenses(req: Request, res: Response) {
     if (!budget) {
       return res
         .status(404)
-        .send("Orçamento para a vigência especificada não encontrado.");
+        .send({
+          res: "Orçamento para a vigência especificada não encontrado.",
+        });
     }
 
     let week: any = budget.weeks.find((w: any) => w.weekNumber === weekNumber);
 
     if (!week) {
-      return res.status(404).send("Semana não encontrada.");
+      return res.status(404).send({ res: "Semana não encontrada." });
     }
 
     if (week.weekRemainingBudget < value) {
-      return res.status(400).send("Orçamento da semana insuficiente.");
+      return res.status(400).send({ res: "Orçamento da semana insuficiente." });
     }
 
     // Adicionando despesa
@@ -77,7 +79,7 @@ export async function AddWeeklyExpenses(req: Request, res: Response) {
 
     res.status(200).send({ message: "Despesa adicionada com sucesso." });
   } catch (error) {
-    console.error("Erro ao adicionar despesa:", error);
-    res.status(500).send("Erro interno do servidor.");
+    console.error({ res: "Erro ao adicionar despesa:", error });
+    res.status(500).send({ res: "Erro interno do servidor." });
   }
 }
